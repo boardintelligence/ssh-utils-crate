922da6cfb1e68eb69bda9874eeebe426e376bdb0
Patrik Sundberg <patrik.sundberg@gmail.com>
2013-08-21 10:37:47 +0100

    update to pallet 0.8.0 RC1.

--- project.clj
+++ project.clj
@@ -5,12 +5,7 @@
             :url "http://boardintelligence.mit-license.org"}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.palletops/pallet "0.8.0-beta.5"]
+                 [com.palletops/pallet "0.8.0-RC.1"]
                  [ch.qos.logback/logback-classic "1.0.7"]]
 
-  :dev-dependencies [[com.palletops/pallet "0.8.0-beta.5" :type "test-jar"]
-                     [com.palletops/pallet-lein "0.6.0-beta.7"]]
-
-  :profiles {:dev
-             {:dependencies [[com.palletops/pallet "0.8.0-beta.5" :classifier "tests"]]
-              :plugins [[com.palletops/pallet-lein "0.6.0-beta.7"]]}})
+  :profiles {:dev {:plugins [[com.palletops/pallet-lein "0.8.0-alpha.1"]]}})
